0cfc35999f04635fa83529657fa173faf773352c
Jorge Armando Sandoval Contreras <j4cksn0w@MacBook-Air-de-Armando.local>
2020-06-20 20:11:11 -0500

    agregar rutas de update y delete

--- routes/post.js
+++ routes/post.js
@@ -8,5 +8,7 @@ var api = express.Router();
 api.post('/newPost',PostController.newPost);
 api.get('/getPost/idPost/:id',PostController.getPost);
 api.get('/getPosts',PostController.getPosts);
+api.post('/updatePost/idPost/:id',PostController.updatePost);
+api.post('/deletePost/idPost/:id',PostController.deletePost);
 
 module.exports = api;
