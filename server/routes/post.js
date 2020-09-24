810b075ca69cc33f16d884ae7bc30a0d00da546d
Jorge Armando Sandoval Contreras <j4cksn0w@MacBook-Air-de-Armando.local>
2020-09-24 16:35:30 -0500

    Agregando autorizacion de sesion en la api

--- server/routes/post.js
+++ server/routes/post.js
@@ -5,11 +5,13 @@ var PostController = require('../controllers/post');
 
 var api = express.Router();
 
+var md_auth = require('../middleware/authenticated');
+
 api.post('/newPost', PostController.newPost);
 api.get('/getPost/idPost/:id', PostController.getPost);
 api.get('/getPosts', PostController.getPosts);
 api.post('/updatePost/idPost/:id', PostController.updatePost);
 api.post('/deletePost/idPost/:id', PostController.deletePost);
-api.get('/getPostsByUser', PostController.getPostByUser);
+api.get('/getPostsByUser', md_auth.ensureAuth, PostController.getPostByUser);
 
 module.exports = api;
